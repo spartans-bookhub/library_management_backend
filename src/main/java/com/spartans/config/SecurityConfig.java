9a8ff4b29e640e9aeca2cc57b1a7301477c8d9bf
Nishu Sinha <your.email@example.com>
2025-10-11 12:39:55 +0530

    entity changes

--- src/main/java/com/spartans/config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.spartans.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@Configuration
-public class SecurityConfig {
-    // TODO: Implement security configuration
-}
