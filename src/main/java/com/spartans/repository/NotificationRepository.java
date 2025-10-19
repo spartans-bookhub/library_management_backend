eff2ef6051ebb3ff3612427a33e85d3c29c7cc0f
nishusinha20 <nishusinha20@gmail.com>
2025-10-19 22:31:31 +0530

    added notification table

---
+++ src/main/java/com/spartans/repository/NotificationRepository.java
@@ -0,0 +1,25 @@
+package com.spartans.repository;
+
+import com.spartans.model.Notification;
+import com.spartans.model.User;
+import com.spartans.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    // Find all notifications for a user
+    List<Notification> findByUser(User user);
+
+    // Find all notifications for a user and book
+    List<Notification> findByUserAndBook(User user, Book book);
+
+    // Find all notifications by status
+    List<Notification> findByStatus(String status);
+
+    // TODO: find recent notifications for a user
+    //  List<Notification> findTop10ByUserOrderByCreatedAtDesc(User user);
+}
