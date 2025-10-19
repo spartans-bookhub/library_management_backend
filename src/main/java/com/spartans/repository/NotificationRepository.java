07024dba10f6f542f0840e526eabdb80e9e0e8fd
nishusinha20 <nishusinha20@gmail.com>
2025-10-19 22:33:43 +0530

    maven spotless apply

--- src/main/java/com/spartans/repository/NotificationRepository.java
+++ src/main/java/com/spartans/repository/NotificationRepository.java
@@ -1,25 +1,24 @@
 package com.spartans.repository;
 
+import com.spartans.model.Book;
 import com.spartans.model.Notification;
 import com.spartans.model.User;
-import com.spartans.model.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    // Find all notifications for a user
-    List<Notification> findByUser(User user);
+  // Find all notifications for a user
+  List<Notification> findByUser(User user);
 
-    // Find all notifications for a user and book
-    List<Notification> findByUserAndBook(User user, Book book);
+  // Find all notifications for a user and book
+  List<Notification> findByUserAndBook(User user, Book book);
 
-    // Find all notifications by status
-    List<Notification> findByStatus(String status);
+  // Find all notifications by status
+  List<Notification> findByStatus(String status);
 
-    // TODO: find recent notifications for a user
-    //  List<Notification> findTop10ByUserOrderByCreatedAtDesc(User user);
+  // TODO: find recent notifications for a user
+  //  List<Notification> findTop10ByUserOrderByCreatedAtDesc(User user);
 }
