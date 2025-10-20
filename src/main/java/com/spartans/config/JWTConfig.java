5dcf71235bd08b04b21b025475507922dbe09a85
Navya Giri <navya.giridharji@gmail.com>
2025-10-20 18:06:24 +0530

    Test cases

--- src/main/java/com/spartans/config/JWTConfig.java
+++ src/main/java/com/spartans/config/JWTConfig.java
@@ -1,9 +1,9 @@
 package com.spartans.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "jwt")
 public class JWTConfig {
   private String secret;
--- src/main/java/com/spartans/dto/PasswordRequestDTO.java
+++ src/main/java/com/spartans/dto/PasswordRequestDTO.java
@@ -5,11 +5,24 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record PasswordRequestDTO(
-    @NotBlank String oldPassword,
     @NotBlank
         @Size(min = 6, message = "Password must be at least 6 characters")
         @Pattern(
             regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$",
             message =
                 "Password must contain at least one digit, one uppercase letter, one lowercase letter, and one special character")
-        String newPassword) {}
+        String oldPassword,
+    @NotBlank
+        @Size(min = 6, message = "Password must be at least 6 characters")
+        @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$",
+            message =
+                "Password must contain at least one digit, one uppercase letter, one lowercase letter, and one special character")
+        String newPassword,
+    @NotBlank
+        @Size(min = 6, message = "Password must be at least 6 characters")
+        @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$",
+            message =
+                "Password must contain at least one digit, one uppercase letter, one lowercase letter, and one special character")
+        String confirmNewPassword) {}
